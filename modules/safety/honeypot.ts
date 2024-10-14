72b2d5aa8c300e11048c8dc5d44b742c655b25f6
mbotboi <mbotboi@protonmail.com>
2024-10-14 10:54:14 +0800

    added honeypot checker as an extra filter

--- modules/safety/honeypot.ts
+++ modules/safety/honeypot.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 import { CHAIN_IDS } from "../constants";
-import { cache } from "../data/cache";
-import { getToken, saveToken } from "../data/mongo/crud/tokenList";
 
 type HoneypotData = {
   chain: string;
@@ -37,48 +35,4 @@ export async function honeypotSol(token: string) {
     );
     return null;
   }
-}
-
-export async function honeypotCheck(
-  token: string,
-  tokenSymbol: string,
-  chain: string
-) {
-  let data: HoneypotData | undefined;
-  let dataInDB = false;
-
-  const tokenListInCache: any[] | undefined = cache.get(`${chain}TokensInDB`);
-  if (tokenListInCache) {
-    data = tokenListInCache!.find((t) => t.address == token);
-    dataInDB = true;
-  } else {
-    const tData = await getToken(token);
-    if (tData) {
-      dataInDB = true;
-      data = tData;
-    }
-  }
-
-  if (!dataInDB) {
-    data = {
-      chain: chain,
-      symbol: tokenSymbol,
-      address: token,
-      honeypot: null,
-    };
-    if (chain == "base" || chain == "ethereum") {
-      const hpData = await honeypotIs(token, CHAIN_IDS[chain]);
-      if (hpData) {
-        if (hpData.honeypotResult)
-          data.honeypot = hpData.honeypotResult.isHoneypot;
-      }
-    } else if (chain == "solana") {
-      data.honeypot = await honeypotSol(token);
-    }
-    if (data) {
-      await saveToken(data.address, data.symbol, data.address, data.honeypot);
-    }
-  }
-  // console.log(data);
-  return data ? data.honeypot : null;
 }
